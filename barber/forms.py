ddedea7f25a0009e5af10b06e094e9262f3f7aeb
Roberta Brandao <roberta@robertabrandao.com.br>
2025-06-01 13:08:11 -0300

    fix: ajustes no label do form

--- barber/forms.py
+++ barber/forms.py
@@ -5,6 +5,12 @@ class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
         fields = ['category', 'name', 'duration', 'price']
+        labels = {
+            'category': 'Categoria', 
+            'name': 'Nome do Serviço',
+            'duration': 'Duração', 
+            'price': 'Preço R$'
+        }
         widgets = {
             'duration': forms.TextInput(attrs={'placeholder': '00:30:00'}),
         }
@@ -14,6 +20,13 @@ class AppointmentForm(forms.ModelForm):
         model = Appointment
         fields = ['client_name','client_cell', 'service', 
                   'appointment_datetime', 'status']
+        labels = {
+            'client_name': 'Nome do Cliente',
+            'client_cell': 'Celular do Cliente', 
+            'service': 'Serviço Prestado', 
+            'appointment_datetime': 'Data e Hora da agenda', 
+            'status': 'Status do Serviço'
+        }
         widgets = {
             'appointment_datetime': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
@@ -22,3 +35,6 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name']
+        labels = {
+            'name': 'Nome'
+        }
